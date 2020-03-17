25fcfc3c2589e97a59d59985bcb97b69c6990ab8
bettergrammer <wiesnerjjason@gmail.com>
2020-03-16 23:09:23 -0700

    Complete initial listings route

--- server/index.js
+++ server/index.js
@@ -12,3 +12,28 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+//get product by unique identifier using req object params
+app.get('/:id', (req, res) => {
+  let id = req.params.id;
+  if (isNaN(Number(id))) {
+    //identifier is name
+    getMainRouteString(id)
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      console.log('err', err);
+    });
+  } else {
+    //identifier is lisitng_id
+    id = Number(id);
+    getMainRouteNum(id)
+    .then((results) => {
+      res.send(results);
+    })
+    .catch((err) => {
+      console.log('err', err);
+    });
+  }
+});
