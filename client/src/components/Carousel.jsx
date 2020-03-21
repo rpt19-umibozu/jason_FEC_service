91e41f50ef1d45d19e06af80609364109e091845
bettergrammer <wiesnerjjason@gmail.com>
2020-03-20 22:26:37 -0700

    Complete carousel caption position

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -5,7 +5,7 @@ class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      nextPrevBorders: ['none', 'none', 'none', 'none'],
+      nextPrevBorders: ['2px solid black', 'none', 'none', 'none'],
       nextPrevOpacities: ['100%', '70%', '70%', '70%']
     };
   }
@@ -47,12 +47,16 @@ class Carousel extends React.Component {
         <img src={`${this.props.state.currentPhoto}`} alt="currently selected carousel photo" id="currentCarouselPhoto"/>
         <img src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/carouselRight.png" alt="carousel right button" id="carouselRightButton"/>
         </div>
-        <div id="prevAndNextImagesContainer">
-          <img src={this.props.state.nextPrevImages[0]} alt='preview of previous and next images in carousel' className='nextPrevImages' onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[0]}`, opacity:`${this.state.nextPrevOpacities[0]}`}} id="0"/>
-          <img src={this.props.state.nextPrevImages[1]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[1]}`, opacity:`${this.state.nextPrevOpacities[1]}`}} id="1"/>
-          <img src={this.props.state.nextPrevImages[2]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[2]}`, opacity:`${this.state.nextPrevOpacities[2]}`}} id="2"/>
-          <img src={this.props.state.nextPrevImages[3]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[3]}`, opacity:`${this.state.nextPrevOpacities[3]}`}} id="3"/>
-        </div>
+          <div id="prevAndNextImagesContainer">
+            <img src={this.props.state.nextPrevImages[0]} alt='preview of previous and next images in carousel' className='nextPrevImages' onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[0]}`, opacity:`${this.state.nextPrevOpacities[0]}`}} id="0"/>
+            <img src={this.props.state.nextPrevImages[1]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[1]}`, opacity:`${this.state.nextPrevOpacities[1]}`}} id="1"/>
+            <img src={this.props.state.nextPrevImages[2]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[2]}`, opacity:`${this.state.nextPrevOpacities[2]}`}} id="2"/>
+            <img src={this.props.state.nextPrevImages[3]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[3]}`, opacity:`${this.state.nextPrevOpacities[3]}`}} id="3"/>
+          </div>
+          <div id="numberAndCaptionContainer">
+          <h4 id="numberOfCurrentPhoto">1 / 30</h4>
+          <p id="caption">The treehouse is a great place for a romantic picnic!</p>
+          </div>
       </div>
     )
   }
