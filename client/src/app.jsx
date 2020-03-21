9f9dd3f254c7e77b445c5e46c5ee1682b647624f
bettergrammer <wiesnerjjason@gmail.com>
2020-03-21 02:05:18 -0700

    Complete mini carousel with full functionality

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -16,7 +16,10 @@ class App extends React.Component {
       currentListing: [],
       is_Favorite: false,
       currentPhoto: null,
-      nextPrevImages: []
+      photoNumber: null,
+      nextPrevImages: [],
+      nextPrevBorders: ['2px solid black', 'none', 'none', 'none'],
+      nextPrevOpacities: ['100%', '70%', '70%', '70%']
     }
 
   }
@@ -47,6 +50,174 @@ class App extends React.Component {
       });
   };
 
+  handleNextPrevClick(e) {
+    e.preventDefault;
+    let src = e.target.src;
+    let srcSplit = src.split('/');
+    let file = srcSplit.pop();
+    file = file.split('');
+    file.splice(-9, 5, 'Large').join('');
+    srcSplit.push(file.join(''));
+    src = srcSplit.join('/');
+    let num = src.split('/').pop();
+    num = num.split('').slice(-11, -9).join('');
+    let id = e.target.id;
+    let photosArray = Object.keys(this.state.currentListing).slice(4, -3);
+    let listingSmallUrls = [];
+    let getNumOfListingPhotos = () => {
+      let count = 0;
+      let listing = this.state.currentListing;
+      console.log('listing', listing);
+      for (let key of photosArray) {
+        console.log('key', key)
+        if (listing[key]) {
+          if (key.split('').slice(-1) == 'b') {
+            listingSmallUrls.push(listing[key])
+          }
+          count++
+        }
+      }
+      return count;
+    }
+    let numOfListingPhotos = Math.ceil(getNumOfListingPhotos() / 3);
+    console.log('numOflistingPhotos', numOfListingPhotos);
+    console.log('listingSmallUrls', listingSmallUrls);
+    let index = listingSmallUrls.indexOf(e.target.src);
+    console.log('index', index);
+    let nextPrevIndex = this.state.nextPrevImages.indexOf(e.target.src);
+    console.log('nextPrevIndex', nextPrevIndex);
+
+    if (nextPrevIndex > 1 && this.state.nextPrevImages[this.state.nextPrevImages.length - 1] !== listingSmallUrls[listingSmallUrls.length - 1]) {
+
+      this.setState({
+        nextPrevImages: this.state.nextPrevImages.map((x, i) => {
+          if (i < 3) {
+            return this.state.nextPrevImages[i + 1];
+          } else {
+            if (nextPrevIndex === 2) {
+              return listingSmallUrls[index + 2];
+            } else {
+              return listingSmallUrls[index + 1];
+            }
+          }
+        })
+      });
+
+      if (nextPrevIndex === 2) {
+        if (index === listingSmallUrls.length - 2) {
+          this.setState({
+            nextPrevBorders: ['none', 'none', '2px solid black', 'none'],
+            nextPrevOpacities: ['70%', '70%', '100%', '70%'],
+            currentPhoto: src,
+            photoNumber: num
+          });
+        } else {
+          this.setState({
+            nextPrevBorders: ['none', '2px solid black', 'none', 'none'],
+            nextPrevOpacities: ['70%', '100%', '70%', '70%'],
+            currentPhoto: src,
+            photoNumber: num
+          });
+        }
+      } else if (nextPrevIndex === 3) {
+        if (index === listingSmallUrls.length - 1) {
+          this.setState({
+            nextPrevBorders: ['none', 'none', 'none', '2px solid black'],
+            nextPrevOpacities: ['70%', '70%', '70%', '100%'],
+            currentPhoto: src,
+            photoNumber: num
+          });
+        } else {
+          this.setState({
+            nextPrevBorders: ['none', 'none', '2px solid black', 'none'],
+            nextPrevOpacities: ['70%', '70%', '100%', '70%'],
+            currentPhoto: src,
+            photoNumber: num
+          });
+        }
+      }
+
+    } else if (nextPrevIndex < 2 && this.state.nextPrevImages[0] !== listingSmallUrls[0]) {
+      this.setState({
+        nextPrevImages: this.state.nextPrevImages.map((x, i) => {
+          if (i > 0) {
+            return this.state.nextPrevImages[i - 1];
+          } else {
+            if (nextPrevIndex === 1) {
+              return listingSmallUrls[index - 2];
+            }
+            return listingSmallUrls[index - 1];
+          }
+        })
+      });
+      if (nextPrevIndex === 1) {
+        this.setState({
+          nextPrevBorders: ['none', 'none', '2px solid black', 'none'],
+          nextPrevOpacities: ['70%', '70%', '100%', '70%'],
+          currentPhoto: src,
+          photoNumber: num
+        });
+      } else {
+        this.setState({
+          nextPrevBorders: ['none', '2px solid black', 'none', 'none'],
+          nextPrevOpacities: ['70%', '100%', '70%', '70%'],
+          currentPhoto: src,
+          photoNumber: num
+        });
+      }
+
+    } else if (nextPrevIndex === 0 && this.state.nextPrevImages[0] === listingSmallUrls[0]) {
+      this.setState({
+        nextPrevBorders: ['2px solid black', 'none', 'none', 'none'],
+        nextPrevOpacities: ['100%', '70%', '70%', '70%'],
+        currentPhoto: src,
+        photoNumber: num
+      });
+    } else if (nextPrevIndex === 1 && this.state.nextPrevImages[0] === listingSmallUrls[0]) {
+      this.setState({
+        nextPrevBorders: ['none', '2px solid black', 'none', 'none'],
+        nextPrevOpacities: ['70%', '100%', '70%', '70%'],
+        currentPhoto: src,
+        photoNumber: num
+      });
+    } else if (nextPrevIndex === 2 && this.state.nextPrevImages[this.state.nextPrevImages.length - 1] === listingSmallUrls[listingSmallUrls.length - 1]) {
+      this.setState({
+        nextPrevBorders: ['none', 'none', '2px solid black', 'none'],
+        nextPrevOpacities: ['70%', '70%', '100%', '70%'],
+        currentPhoto: src,
+        photoNumber: num
+      });
+    } else if (nextPrevIndex === 3 && this.state.nextPrevImages[this.state.nextPrevImages.length - 1] === listingSmallUrls[listingSmallUrls.length - 1]) {
+      this.setState({
+        nextPrevBorders: ['none', 'none', 'none', '2px solid black'],
+        nextPrevOpacities: ['70%', '70%', '70%', '100%'],
+        currentPhoto: src,
+        photoNumber: num
+      });
+    } else {
+      let newBorders = ['none', 'none', 'none', 'none'].map((x, i) => {
+        if (i == id - 1) {
+          return '2px solid black';
+        } else {
+          return x;
+        }
+      });
+      let newOpacities = ['70%', '70%', '70%', '70%'].map((x, i) => {
+        if (i == id - 1) {
+          return '100%';
+        } else {
+          return x;
+        }
+      });
+      this.setState({
+        nextPrevBorders: newBorders,
+        nextPrevOpacities: newOpacities,
+        currentPhoto: src,
+        photoNumber: num
+      });
+    }
+  };
+
   handleSearchBar (e) {
     if (e.key === 'Enter') {
       e.preventDefault();
@@ -54,12 +225,19 @@ class App extends React.Component {
     }
   }
 
+  handleExit(e) {
+    e.preventDefault;
+    this.setState({
+      carousel: Empty
+    });
+  };
+
   render() {
     return (
       <div id="photoGalleryService">
         <this.state.navBar handleSearchBar={this.handleSearchBar.bind(this)}/>
         <this.state.photoGallery state={this.state}/>
-        <this.state.carousel state={this.state}/>
+        <this.state.carousel state={this.state} handleExit={this.handleExit.bind(this)} handleNextPrevClick={this.handleNextPrevClick.bind(this)}/>
       </div>
     )
   }
--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -4,54 +4,22 @@ import $ from 'jquery';
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      nextPrevBorders: ['2px solid black', 'none', 'none', 'none'],
-      nextPrevOpacities: ['100%', '70%', '70%', '70%']
-    };
-  }
-
-  handleExit(e) {
-    e.preventDefault;
-    console.log('handling exit...')
-  }
-
-  handleNextPrevClick(e) {
-    e.preventDefault;
-    let id = e.target.id;
-    let newBorders = ['none', 'none', 'none', 'none'].map((x, i) => {
-      if (i == id) {
-        return '2px solid black';
-      } else {
-        return x;
-      }
-    });
-    let newOpacities = ['70%', '70%', '70%', '70%'].map((x, i) => {
-      if (i == id) {
-        return '100%';
-      } else {
-        return x;
-      }
-    });
-      this.setState({
-        nextPrevBorders: newBorders,
-        nextPrevOpacities: newOpacities
-      });
   }
 
   render() {
     return (
       <div id="carouselContainer">
-        <img onClick={this.handleExit} src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/carouselX.png" alt="carousel exit button" id="carouselExitButton"/>
+        <img onClick={this.props.handleExit.bind(this)} src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/carouselX.png" alt="carousel exit button" id="carouselExitButton"/>
         <div style={{float:'left'}}>
         <img src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/carouselLeft.png" alt="carousel left button" id="carouselLeftButton"/>
         <img src={`${this.props.state.currentPhoto}`} alt="currently selected carousel photo" id="currentCarouselPhoto"/>
         <img src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/carouselRight.png" alt="carousel right button" id="carouselRightButton"/>
         </div>
           <div id="prevAndNextImagesContainer">
-            <img src={this.props.state.nextPrevImages[0]} alt='preview of previous and next images in carousel' className='nextPrevImages' onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[0]}`, opacity:`${this.state.nextPrevOpacities[0]}`}} id="0"/>
-            <img src={this.props.state.nextPrevImages[1]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[1]}`, opacity:`${this.state.nextPrevOpacities[1]}`}} id="1"/>
-            <img src={this.props.state.nextPrevImages[2]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[2]}`, opacity:`${this.state.nextPrevOpacities[2]}`}} id="2"/>
-            <img src={this.props.state.nextPrevImages[3]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.handleNextPrevClick.bind(this)} style={{border:`${this.state.nextPrevBorders[3]}`, opacity:`${this.state.nextPrevOpacities[3]}`}} id="3"/>
+            <img src={this.props.state.nextPrevImages[0]} alt='preview of previous and next images in carousel' className='nextPrevImages' onClick={this.props.handleNextPrevClick} style={{border:`${this.props.state.nextPrevBorders[0]}`, opacity:`${this.props.state.nextPrevOpacities[0]}`}} id="0"/>
+            <img src={this.props.state.nextPrevImages[1]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.props.handleNextPrevClick} style={{border:`${this.props.state.nextPrevBorders[1]}`, opacity:`${this.props.state.nextPrevOpacities[1]}`}} id="1"/>
+            <img src={this.props.state.nextPrevImages[2]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.props.handleNextPrevClick} style={{border:`${this.props.state.nextPrevBorders[2]}`, opacity:`${this.props.state.nextPrevOpacities[2]}`}} id="2"/>
+            <img src={this.props.state.nextPrevImages[3]} alt='preview of previous and next images in carousel' className='nextPrevImages'onClick={this.props.handleNextPrevClick} style={{border:`${this.props.state.nextPrevBorders[3]}`, opacity:`${this.props.state.nextPrevOpacities[3]}`}} id="3"/>
           </div>
           <div id="numberAndCaptionContainer">
           <h4 id="numberOfCurrentPhoto">1 / 30</h4>
