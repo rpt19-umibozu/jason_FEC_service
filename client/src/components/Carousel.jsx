16f1c6fc8bc8ab9a5ccc10d949917ec20e0d3cd2
bettergrammer <wiesnerjjason@gmail.com>
2020-03-20 18:34:48 -0700

    Complete carousel right button placement

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -6,13 +6,19 @@ class Carousel extends React.Component {
     super(props);
     this.state = {};
   }
+
+  handleExit(e) {
+    e.preventDefault;
+    console.log('handling exit...')
+  }
+
   render() {
     return (
       <div id="carouselContainer">
-        <img src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/carouselX.png" alt="carousel exit button" id="carouselExitButton"/>
+        <img onClick={this.handleExit} src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/carouselX.png" alt="carousel exit button" id="carouselExitButton"/>
         <img src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/carouselLeft.png" alt="carousel left button" id="carouselLeftButton"/>
         <img src={`${this.props.state.currentListing.photo1_a}`} alt="currently selected carousel photo" id="currentCarouselPhoto"/>
-        {/* <img src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/carouselRight.png" alt="carousel right button" id="carouselRightButton"/> */}
+        <img src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/carouselRight.png" alt="carousel right button" id="carouselRightButton"/>
       </div>
     )
   }
