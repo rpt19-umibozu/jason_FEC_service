b23483d3e339e11cb6bf11df86b6f4ad3b269b8a
bettergrammer <wiesnerjjason@gmail.com>
2020-03-29 18:12:46 -0700

    Rename App to PhotoService

---
+++ client/src/renderPhotoService.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PhotoService from './PhotoService.jsx';
+
+ReactDOM.render(<PhotoService/>, document.getElementById('PhotoService'));
