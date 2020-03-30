14de9c9cfe778e0982f81ce27f497fadd17c132a
bettergrammer <wiesnerjjason@gmail.com>
2020-03-30 03:49:45 -0700

    Complete photoGallery opacities, hardcode photos

---
+++ client/src/components/HardcodePhotos.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class HardcodePhotos extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <img id="hardcodePhoto1" src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/listingTitleHardcoded.png"/>
+        <img id="hardcodePhoto2" src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/listingTitleHardcoded2.png"/>
+        <img id="hardcodePhoto3" src="https://fec-photos.s3-us-west-1.amazonaws.com/otherPics/listingTitleHardcoded3.png"/>
+      </div>
+    )
+  }
+}
+
+export default HardcodePhotos;
