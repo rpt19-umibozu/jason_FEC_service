eec4816cb6e2045f28f0fa14147c462c8b2ba964
bettergrammer <wiesnerjjason@gmail.com>
2020-04-11 17:34:30 -0700

    Fix buttons

--- client/src/components/PhotoGallery.jsx
+++ client/src/components/PhotoGallery.jsx
@@ -17,9 +17,9 @@ class PhotoGallery extends React.Component {
       heartSize: '24px',
       heartPosition: '10px 6px',
       shareButtonMargin: '23px 0 0 130px',
-      shareBottom: '625px',
+      shareBottom: '610px',
       shareRight: '131px',
-      saveBottom: '625px',
+      saveBottom: '610px',
       saveLeft: '1308px',
       photoOpacities: ['1-index-array', '100%', '100%', '100%', '100%', '100%']
     };
