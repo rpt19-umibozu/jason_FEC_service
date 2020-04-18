052dad6aee809306297cf250acb5295967eaeebd
bettergrammer <wiesnerjjason@gmail.com>
2020-04-18 14:13:44 -0700

    Add terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const webpack = require('webpack');
 const BrotliPlugin = require('brotli-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: __dirname + '/client/src/renderPhotoService.jsx',
@@ -36,6 +37,17 @@ module.exports = {
 			threshold: 10240,
 			minRatio: 0.8
     }),
-    new webpack.optimize.UglifyJsPlugin()
-	]
+  ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true, // Must be set to true if using source-maps in production
+        terserOptions: {
+          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
+        }
+      }),
+    ],
+  }
 };
